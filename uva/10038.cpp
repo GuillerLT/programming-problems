6c04ebf70c909a267f25127fbe226c34ac93b27c
Guillermo Lloret Talavera <GuillerLT@gmail.com>
2019-07-18 23:59:00 +0000

    2019.07.18

---
+++ uva/10038.cpp
@@ -0,0 +1,29 @@
+#include <cstdio>
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  int n;
+  while (scanf("%d", &n) == 1) {
+    bool jolly = true;
+    vector<char> vc(n, 0);
+    int prev;
+    cin >> prev;
+    for (int i = 1; i < n; ++i) {
+      int next;
+      cin >> next;
+      int const diff = abs(next - prev);
+      if (diff >= n || vc[diff] != 0) {
+        jolly = false;
+      } else {
+        vc[diff] = 1;
+      }
+      prev = next;
+    }
+    cout << (jolly ? "Jolly\n" : "Not jolly\n");
+  }
+  return 0;
+}
---
+++ uva/11340.cpp
@@ -0,0 +1,32 @@
+#include <cstdio>
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int t;
+  scanf("%d\n", &t);
+  while (--t >= 0) {
+    long long prices[256];
+    for (int i = 0; i < 256; ++i) prices[i] = 0LL;
+    int n;
+    scanf("%d\n", &n);
+    while (--n >= 0) {
+      unsigned char c;
+      long long p;
+      scanf("%c %lld\n", &c, &p);
+      prices[c] = p;
+    }
+    scanf("%d\n", &n);
+    long long total = 0LL;
+    while (--n >= 0) {
+      string l;
+      getline(cin, l);
+      for (unsigned char const& c : l) {
+        total += prices[c];
+      }
+    }
+    printf("%lld.%02lld$\n", total / 100, total % 100);
+  }
+  return 0;
+}

3e178b905a35794f689df75adc37a923f6304d47
Guillermo Lloret Talavera <GuillerLT@gmail.com>
2019-10-07 23:59:00 +0000

    2019.10.07

---
+++ codeforces/00023/A.cpp
@@ -0,0 +1,23 @@
+#include <array>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  array<int, 3001> pfactors;
+  pfactors.fill(0);
+  int n;
+  cin >> n;
+  int cnt = 0;
+  for (int i = 2; i <= n; ++i) {
+    if (pfactors[i] == 0) {
+      for (int j = i + i; j <= n; j += i) {
+        ++pfactors[j];
+      }
+    } else if (pfactors[i] == 2) {
+      ++cnt;
+    }
+  }
+  cout << cnt << "\n";
+  return 0;
+}

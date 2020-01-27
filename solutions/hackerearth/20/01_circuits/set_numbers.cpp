5b98e7c2d59963db6aadf47c618c6b360a840748
Guillermo Lloret Talavera <GuillerLT@gmail.com>
2020-01-27 23:59:00 +0000

    2020.01.27

---
+++ solutions/hackerearth/20/01_circuits/set_numbers.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    ++n;
+    int cnt = 0;
+    while(n >>= 1) {
+      ++cnt;
+    }
+    cout << ((~0U) >> (sizeof(int) * 8 - cnt)) << endl;
+  }
+  return 0;
+}

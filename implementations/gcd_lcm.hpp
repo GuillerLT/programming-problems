41237aec21994ad1d5bcfecb594313e9182f4a24
Guillermo Lloret Talavera <GuillerLT@gmail.com>
2020-02-17 23:59:00 +0000

    GCD and LCM fix

--- implementations/gcd_lcm.hpp
+++ implementations/gcd_lcm.hpp
@@ -5,17 +5,17 @@ namespace glt {
 
 template <typename T>
 T gcd(T a, T b) {
-  while (b != static_cast<T>(0)) {
+  while (b != static_cast<T>(0LL)) {
     T const c = a % b;
     a = b;
     b = c;
   }
-  return a;
+  return abs(a);
 }
 
 template <typename T>
 T lcm(T const a, T const b) {
-  return a * b / gcd(a, b);
+  return abs(a * (b / gcd(a, b)));
 }
 
 }  // namespace glt

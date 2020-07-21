44ced4ed5b2cafe96f60b8d52f8f6ab61df9d886
Guillermo Lloret Talavera <GuillerLT@gmail.com>
2020-07-21 23:59:00 +0000

    2020.07.21

---
+++ solutions/codeforces/00903/a.cpp
@@ -0,0 +1,34 @@
+/* Problemset 903 - Problem A
+ * https://codeforces.com/problemset/problem/903/A
+ */
+
+#include <array>
+#include <iostream>
+
+using namespace std;
+
+template <typename T>
+T get() {
+  T t;
+  cin >> t;
+  return t;
+}
+
+int main() {
+  array<bool, 3> playing{1, 1, 0};
+  for (int n = get<int>(); n > 0; --n) {
+    int const winner = get<int>() - 1;
+    for (int i = 0; i < 3; ++i) {
+      if (i == winner) {
+        if (!playing[winner]) {
+          cout << "NO\n";
+          return 0;
+        }
+      } else {
+        playing[i] = !playing[i];
+      }
+    }
+  }
+  cout << "YES\n";
+  return 0;
+}

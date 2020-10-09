d2726a898962f8e3565d4f7617bf10c785341be3
Guillermo Lloret Talavera <GuillerLT@gmail.com>
2020-10-09 19:24:56 +0200

    Changed BIT from class to free functions
    Changed Binary Indexed [Fenwick] Tree (BIT) implementation from class to a serie of free functions that receive iterators as parameters (allowing the user to use any container)

--- implementations/binary_indexed_tree.hpp
+++ implementations/binary_indexed_tree.hpp
@@ -1,48 +1,50 @@
 #ifndef glt_binary_indexed_tree
 #define glt_binary_indexed_tree
 
-#include <utility>
-#include <vector>
+#include <algorithm>
+#include <iterator>
 
 namespace glt {
 
-/* Binary Indexed (Fenwick) Tree class
+/* Binary Indexed (Fenwick) Tree
  * http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.14.8917&rep=rep1&type=pdf
  */
-template <typename T = int>
-class binary_indexed_tree {
- private:
-  std::vector<T> values;
 
-  T _query(size_t rquery) {
-    T value = static_cast<T>(0);
-    while (rquery > 0) {
-      value += values[rquery];
-      rquery -= (rquery & -rquery);
-    }
-    return value;
+template <typename ForwardIt, typename T>
+constexpr auto update_binary_indexed_tree(ForwardIt const first,
+                                          ForwardIt const pos,
+                                          ForwardIt const last, T const delta)
+    -> void {
+  auto const size = std::distance(first, last);
+  for (auto index = std::distance(first, pos) + 1; index <= size;
+       index += (index & -index)) {
+    *std::next(first, index - 1) += delta;
   }
-  void _update(size_t index, T const delta) {
-    while (index < values.size()) {
-      values[index] += delta;
-      index += (index & -index);
-    }
-  }
+}
 
- public:
-  binary_indexed_tree(size_t const n) : values(n + 1, static_cast<T>(0)) {}
-  // Preconditions:
-  // - 0 <= lquery <= rquery < size
-  T query(size_t const lquery, size_t const rquery) {
-    return _query(rquery + 1) - _query(lquery);
+template <typename ForwardIt>
+constexpr auto query_binary_indexed_tree(ForwardIt const first,
+                                         ForwardIt const last) ->
+    typename std::iterator_traits<ForwardIt>::value_type {
+  auto query =
+      static_cast<typename std::iterator_traits<ForwardIt>::value_type>(0);
+  for (auto index = std::distance(first, last); index > 0;
+       index -= (index & -index)) {
+    query += *std::next(first, index - 1);
   }
-  // Preconditions:
-  // - 0 <= rquery < size
-  T query(size_t const rquery) { return _query(rquery + 1); }
-  // Preconditions:
-  // - 0 <= index < size
-  void update(size_t const index, T const delta) { _update(index + 1, delta); }
-};
+  return query;
+}
+
+template <typename InputIt, typename OutputIt>
+constexpr void make_binary_indexed_tree(InputIt first, InputIt const last,
+                                        OutputIt const d_first) {
+  auto const size = std::distance(first, last);
+  auto const d_last = std::next(d_first, size);
+  std::fill(d_first, d_last, 0);
+  for (auto d_pos = d_first; d_pos != d_last; ++first, ++d_pos) {
+    update_binary_indexed_tree(d_first, d_pos, d_last, *first);
+  }
+}
 
 }  // namespace glt
 

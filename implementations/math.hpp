0a9af4fae57302a0d6f124345938731479b04bdb
Guillermo Lloret Talavera <GuillerLT@gmail.com>
2021-04-01 14:06:57 +0200

    Implemented binary exponentiation (exponentiation by squares) method

--- implementations/math.hpp
+++ implementations/math.hpp
@@ -15,6 +15,18 @@ constexpr auto sign(T const t) -> std::make_signed_t<T> {
   return t < 0 ? -1 : t > 0 ? +1 : 0;
 }
 
+template <class Integral1, class Integral2>
+constexpr auto int_pow(Integral1 i1, Integral2 i2)
+    -> std::common_type_t<Integral1, Integral2> {
+  std::common_type_t<Integral1, Integral2> ans{1};
+  while (i2 > 0) {
+    if (i2 & 1) ans *= i1;
+    i1 *= i1;
+    i2 >>= 1;
+  }
+  return ans;
+}
+
 }  // namespace glt
 
 #endif
